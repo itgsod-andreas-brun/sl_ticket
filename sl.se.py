b505ab95590e4a61151f62642b299873a4bdead8
itgsod-andreas-brun <andreas.brun@elev.it-gymnasiet.se>
2017-10-25 13:50:01 +0200

    SL ticket klar

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,23 @@
-def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+def fares(age, student=False, senior=False, adult=False, discount=False):
+    if age > 20 and age < 65:
+        if adult==True:
+            return "Fullprice 30kr"
+        else:
+            return "discount price 20kr"
+    elif age <= 20 or age >=65:
+        if student==True:
+            return "discount price 20kr"
+        elif senior==True:
+            return "discount price 20kr"
+
+        print "error"
 
 
 
 
 
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+
+print fares(10)
+print fares(65,senior=True)
+print fares(25,student=True)
+print fares(64,adult=True)
